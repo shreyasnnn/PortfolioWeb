92ffe489655b44363705b35390be4fa87ab76014
shreyasnnn <nshreyas1309@gmail.com>
2025-06-18 20:40:57 +0530

    tailwind configuration tested

--- src/screens/homeScreen/index.tsx
+++ src/screens/homeScreen/index.tsx
@@ -1,13 +1,14 @@
-import {webData} from '../../dataController'
-import {Button} from '../../components/button'
+import { webData } from "../../dataController";
+import { Button } from "../../components/button";
 export const HomeScreen = () => {
   return (
     <>
       <div>
-        <h1 className="text-amber-800 text-5xl ">Home</h1>
-        <h1 className='text-4xl'>{webData.name}</h1>
-        <Button label='primary' size='md'/>
-        
+        <h1 style={{ color: "var(--color-random5)" }} className="text-5xl">
+          Home
+        </h1>
+        <h1 className="text-4xl">{webData.name}</h1>
+        <Button label="primary" size="md" />
       </div>
     </>
   );
