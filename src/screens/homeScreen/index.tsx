25786a5f79c2301b5ad8f1b2b703f024b424b8b8
shreyasnnn <nshreyas1309@gmail.com>
2025-06-20 17:02:39 +0530

    styling using tailwind

--- src/screens/homeScreen/index.tsx
+++ src/screens/homeScreen/index.tsx
@@ -1,13 +1,14 @@
 import { webData } from "../../dataController";
 import { Button } from "../../components/button";
+import '../../index.css'
 export const HomeScreen = () => {
   return (
     <>
       <div>
-        <h1 style={{ color: "var(--color-random5)" }} className="text-5xl">
+        <h1  className="text-5xl text-[var(--color-random1)]">
           Home
         </h1>
-        <h1 className="text-4xl">{webData.name}</h1>
+        <h1 className="text-4xl text-[var(--color-random5)]">{webData.name}</h1>
         <Button label="primary" size="md" />
       </div>
     </>
