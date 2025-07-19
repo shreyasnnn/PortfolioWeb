f4fc290858a40f2d8ce42501a1ac633d4ee7db61
shreyasnnn <nshreyas1309@gmail.com>
2025-07-20 00:07:38 +0530

    responsive

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -10,15 +10,14 @@ import {
 } from "../dataController";
 export default function NavBar() {
   const [onMouseEnter, setOnMouseEnter] = useState(false);
-  
 
   const handleClick = (url: string) => {
-    window.open(url,'_blank')
-  }
+    window.open(url, "_blank");
+  };
 
   return (
     <div className="w-full flex justify-center mt-10">
-      <div className="flex flex-row gap-6 items-center">
+      <div className="flex flex-wrap flex-col lg:flex-row gap-6 items-center">
         <Button
           variant="secondary"
           className="rounded-full p-0 shadow-card h-20 w-20"
@@ -28,15 +27,15 @@ export default function NavBar() {
 
         <a
           href={resume}
-          className="w-64 flex flex-col relative"
+          className="w-64 flex flex-col relative items-center justify-center text-center lg:text-start lg:items-start"
           onMouseEnter={() => setOnMouseEnter(true)}
           onMouseLeave={() => setOnMouseEnter(false)}
         >
-          <span className="font-bold block text-xl">{name}</span>
+          <span className="font-semibold text-title-xs block ">{name}</span>
 
-          <div className="relative h-10 mt-1 flex items-center justify-center">
+          <div className="relative h-10 mt-1 w-full flex items-center justify-center text-center lg:text-start ">
             <p
-              className={`absolute top-0 left-0 w-full transition-opacity duration-300 ${
+              className={`text-caption-s font-regular absolute top-0 left-0 w-full transition-opacity duration-300 ${
                 onMouseEnter ? "opacity-0" : "opacity-100"
               }`}
             >
@@ -56,24 +55,24 @@ export default function NavBar() {
           {NAV_BAR_ITEM.ICON_CONFIG.map((button, index) => {
             return (
               <Button
-              key={index}
+                key={index}
                 variant="secondary"
-                className={`${button.TITLE && "mx-6"} rounded-xl h-16 min-w-16 shadow-card border-0`}
-                onClick={()=>handleClick(button.REDIRECT)}
-              > <div className="flex" >
-                  {
-                    button.IS_ACTIVE && <div >🟢</div>
-                  }
-                  {
-                    button.ICON && <img
-                  className="h-5"
-                  src={button.ICON.URL}
-                  alt={button.ICON.ALT}
-                />
-                  }
-                  {
-                    button.TITLE && <div>{button.TITLE}</div>
-                  }
+                className={`${
+                  button.TITLE && "mx-6"
+                } rounded-xl h-16 min-w-16 shadow-card border-0`}
+                onClick={() => handleClick(button.REDIRECT)}
+              >
+                {" "}
+                <div className="flex">
+                  {button.IS_ACTIVE && <div>🟢</div>}
+                  {button.ICON && (
+                    <img
+                      className="h-5"
+                      src={button.ICON.URL}
+                      alt={button.ICON.ALT}
+                    />
+                  )}
+                  {button.TITLE && <div>{button.TITLE}</div>}
                 </div>
               </Button>
             );
--- src/screens/homeScreen/index.tsx
+++ src/screens/homeScreen/index.tsx
@@ -10,7 +10,7 @@ export const HomeScreen = () => {
       <NavBar />
 
       {/* Scrollable skills section */}
-      <div className="mt-25 w-[30%] mx-auto overflow-hidden  [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+      <div className=" mt-25 lg:w-[30%] w-[80%] mx-auto overflow-hidden  [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
         <div className="flex space-x-4 w-max  animate-scroll no-scrollbar opacity-50">
           {skills.map((skill, index) => (
             <SlideItem
@@ -31,12 +31,12 @@ export const HomeScreen = () => {
       <div className="mt-20 flex items-center justify-center w-full">
         <Button className="relative rounded-3xl text-2xl px-10 py-3 overflow-hidden group text-white">
           {/* Default text */}
-          <span className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 opacity-100 group-hover:opacity-0">
+          <span className="text-body-s font-use-semibold absolute inset-0 flex items-center justify-center transition-opacity duration-300 opacity-100 group-hover:opacity-0">
             Projects
           </span>
 
           {/* Hover text */}
-          <span className="opacity-0 group-hover:opacity-100 transition-color duration-300">
+          <span className="text-body-s font-use-semibold opacity-0 group-hover:opacity-100 transition-color duration-300">
             Let's go
           </span>
         </Button>
